363dc65d45160c2852497f056975f38d155f792d
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-21 12:14:20 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -4,6 +4,7 @@ import os
 
 
 g = Github(os.environ["GITHUB_TOKEN"])
-repo = g.get_repo("stocklui/DiP-iT-Feedback-Test")
+repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
+print(repo)
 
 print("test file")

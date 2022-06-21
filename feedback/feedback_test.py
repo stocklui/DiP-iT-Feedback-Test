004d6901f5560e013007d767d48acfd7267909e8
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-21 15:54:57 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -6,8 +6,10 @@ import os
 g = Github(os.environ["GITHUB_TOKEN"])
 repository = g.get_repo(os.environ["GITHUB_REPOSITORY"])
 print(os.environ["WIKI_DIR"])
+print(os.environ["GITHUB_REPOSITORY"])
 #print(os.environ["WIKI_DIR"]+"feedback.md")
 #print(os.environ["GITHUB_REPOSITORY"]+"/"+os.environ["WIKI_DIR"]+"feedback.md")
+
 #repository.create_file(os.environ["WIKI_DIR"]+"feedback.md", "init commit", "add wiki page")
 
 #file_content = repo.get_contents(os.environ["WIKI_DIR"]+"feedback.md")

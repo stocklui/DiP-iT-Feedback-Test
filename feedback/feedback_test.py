11f40c79fde4a3739de918800b48814c1ed957b8
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-21 13:59:16 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -5,7 +5,7 @@ import os
 
 g = Github(os.environ["GITHUB_TOKEN"])
 repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-repo.create_file("/wiki/testwiki.md", "init commit", "#First Test \nfirst test pushing to wiki")
+repo.create_file("wiki/testwiki.md", "init commit", "#First Test \nfirst test pushing to wiki")
 file_content = repo.get_contents('wiki/testwiki.md')
 
 print("file_content")

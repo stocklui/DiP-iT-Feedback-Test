ee1378c05aa962749e8d6365d507bb95e1dac97d
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-21 15:37:50 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -6,7 +6,7 @@ import git
 
 g = Github(os.environ["GITHUB_TOKEN"])
 repository = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-repository.create_file("tmp/", "init commit", "add tmp directory")
+repository.create_file("tmp/tmp.txt", "init commit", "add tmp directory")
 repo = git.Repo.clone_from("https://github.com/"+os.environ["GITHUB_REPOSITORY"]+".wiki.git", "https://github.com/"+os.environ["GITHUB_REPOSITORY"]+"\tmp")
 repo.index.add(["feedback_wiki"])
 repo.index.commit("try push to wiki")

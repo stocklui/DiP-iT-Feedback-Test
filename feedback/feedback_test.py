f5a6d91290a94c6f99b49d01c273ca39499f4491
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-21 16:21:44 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -5,8 +5,9 @@ import os
 
 g = Github(os.environ["GITHUB_TOKEN"])
 repository = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-wiki = g.get_repo(os.environ["GITHUB_REPOSITORY"]+".wiki.git")
-print(wiki)
+print(os.environ["GITHUB_REPOSITORY"])
+#wiki = g.get_repo(os.environ["GITHUB_REPOSITORY"]+".wiki.git")
+print(os.environ["GITHUB_REPOSITORY"]+".wiki.git")
 contents = repository.get_contents("")
 while contents:
   file_content = contents.pop(0)

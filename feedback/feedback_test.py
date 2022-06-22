dedc63e135805f38d37aff988539d528614ed935
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-22 22:32:28 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -8,5 +8,6 @@ contents = repo.get_contents("README.md")
 repo.update_file(contents.path, "update readme", '#Getting Feedback now...', contents.sha, branch="main")
 # getting feedback and writing into following file
 repo.create_file("wiki/feedback.md", "add tmp feedback", "this should be feedback information")
+#
 
 

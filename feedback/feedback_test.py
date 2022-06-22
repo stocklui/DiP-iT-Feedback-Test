8d03e282b406c95606ed25cd4b9286c89c0bd2ab
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-22 22:57:26 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -8,7 +8,17 @@ repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
 contents = repo.get_contents("README.md")
 repo.update_file(contents.path, "update readme", '#Getting Feedback now...', contents.sha, branch="main")
 # getting feedback and writing into following file
-repo.create_file("wiki/feedback.md", "add tmp feedback", "this should be feedback information")
+all_files = []
+contents1 = repo.get_contents("")
+while contents1:
+    file_content = contents1.pop(0)
+    if file_content.type == "dir":
+        contents1.extend(repo.get_contents(file_content.path))
+    else:
+        file = file_content
+        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+print(all_files)
+#repo.create_file("wiki/feedback.md", "add tmp feedback", "this should be feedback information")
 time.sleep(5)
 #
 

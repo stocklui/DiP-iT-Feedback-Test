dd5b35da02ea8c4ea0e045f186e18580d75bfa57
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-22 22:09:30 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -4,22 +4,9 @@ import os
 g = Github(os.environ["GITHUB_TOKEN"])
 
 repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-#repo.create_file("test_create_file/test.md", "add testfile", "test 1")
 contents = repo.get_contents("README.md")
-print(contents)
-repo.update_file(contents.path, "update readme", "\\n new content added by script", contents.sha, branch="main")
-#contents = repo.get_contents("")
-#print(contents)
+repo.update_file(contents.path, "update readme", '#Getting Feedback now...', contents.sha, branch="main")
+# getting feedback and writing into following file
+repo.create_file("wiki/feedback.md", "add tmp feedback", "this should be feedback information")
 
-#repo.create_file("wiki/feedback.md", "add/update wiki doc", '#Add wiki pagec\n content1', branch="main")
-
-# repo.update_file(contents.path, "more tests", "more tests", contents.sha, branch="test")
-
-#contents = repo.get_contents("")
-#while contents:
-#  file_content = contents.pop(0)
-#  if file_content.type == "dir":
-#    contents.extend(repository.get_contents(file_content.path))
-#  else:
-#    print(file_content)
 

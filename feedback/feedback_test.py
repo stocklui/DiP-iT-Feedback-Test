4d4345c81c93f8ea14372de0b6071a1bbc229d71
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-22 10:18:21 +0200

    Update feedback_test.py

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -6,11 +6,11 @@ g = Github(os.environ["GITHUB_TOKEN"])
 repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
 repo.create_file("wiki/feedback.md", "add/update wiki doc", '#Add wiki pagec\n content1', branch="main")
 
-contents = repo.get_contents("")
-while contents:
-  file_content = contents.pop(0)
-  if file_content.type == "dir":
-    contents.extend(repository.get_contents(file_content.path))
-  else:
-    print(file_content)
+#contents = repo.get_contents("")
+#while contents:
+#  file_content = contents.pop(0)
+#  if file_content.type == "dir":
+#    contents.extend(repository.get_contents(file_content.path))
+#  else:
+#    print(file_content)
 

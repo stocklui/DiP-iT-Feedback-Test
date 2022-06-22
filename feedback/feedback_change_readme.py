1135af2c247ef7c8749f2848d56d1670874000cf
stocklui <88967166+stocklui@users.noreply.github.com>
2022-06-22 23:40:31 +0200

    Update feedback_change_readme.py

--- feedback/feedback_change_readme.py
+++ feedback/feedback_change_readme.py
@@ -8,5 +8,7 @@ contents = repo.get_contents("README.md")
 print(contents)
 new_file_content = contents.decoded_content.decode()
 print(new_file_content)
-new_file_content.replace("#Getting Feedback now...", 'https://github.com/'+os.environ["GITHUB_REPOSITORY"]+'/wiki')
+new_file_content.replace("#Getting Feedback now...", "feedback can be found in wiki")
+print(new_file_conten)
 repo.update_file(contents.path, "update readme",str(new_file_content), contents.sha, branch="main")
+#'https://github.com/'+os.environ["GITHUB_REPOSITORY"]+'/wiki'

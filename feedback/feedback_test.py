7f93f0e7db4bc917f25f6a2fc8cffc35c2d18e0b
stocklui <stocklui@hu-berlin.de>
2022-06-23 13:57:14 +0200

    change markdown

--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -28,10 +28,7 @@ if "README.md" in all_files:
     repo.update_file(contents.path, "update readme", new_message, contents.sha, branch="main")
 
 else:
-    repo.create_file("README.md", "create README", 
-                     "# Feedback  
-                     
-                     ### Getting Feedback now...  ", branch="main")
+    repo.create_file("README.md", "create README", "# Feedback \\n \\n ### Getting Feedback now...  ", branch="main")
 
 
 os.mkdir("../feedback")

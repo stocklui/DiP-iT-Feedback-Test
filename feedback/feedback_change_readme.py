60837a0517247ed45ebd455da717c8239dc4c7c1
stocklui <stocklui@hu-berlin.de>
2022-06-23 13:00:41 +0200

    try to fix markdown

--- feedback/feedback_change_readme.py
+++ feedback/feedback_change_readme.py
@@ -11,4 +11,3 @@ print(new_file_content)
 new_message = new_file_content.replace("Getting Feedback now...",'Click here to get your Feedback -> [Feedback](../../wiki/feedback)')
 print(new_file_content)
 repo.update_file(contents.path, "update readme",new_message, contents.sha, branch="main")
-#'https://github.com/'+os.environ["GITHUB_REPOSITORY"]+'/wiki'
--- feedback/feedback_test.py
+++ feedback/feedback_test.py
@@ -7,12 +7,12 @@ g = Github(os.environ["GITHUB_TOKEN"])
 repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
 contents = repo.get_contents("README.md")
 file_content = contents.decoded_content.decode()
-if ("[Feedback](../../wiki/feedback)" and " ##Feedback Section ") in file_content:
+if ("[Feedback](../../wiki/feedback)" and "Feedback Section") in file_content:
     print("readme already initialized")
     new_message = file_content.replace("Click here to get your Feedback -> [Feedback](../../wiki/feedback)",'Getting Feedback now...')
 else:
     print("readme not initialized")
-    new_message = file_content + '  ##Feedback Section  Getting Feedback now...'
+    new_message = file_content + "    ## Feedback Section    Getting Feedback now...  ""
 repo.update_file(contents.path, "update readme", new_message, contents.sha, branch="main")
 
 os.mkdir("../feedback")
